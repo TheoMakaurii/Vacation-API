6f708ea1eb096a54efc2eb46ef804e8c791310fb
Theo McCauley <TheoMakaurii@gmail.com>
2020-08-18 14:20:13 -0400

    populated dom with park names

--- index.js
+++ index.js
@@ -1,30 +1,66 @@
 
-const url = `https://developer.nps.gov/api/v1/parks?parkCode=acad&api_key=MMqVQEXEFsuN6DmjKGPbOnM7cTqe6hn94ekdewXy`
 
-function main() {
-    console.log('DOM is loaded');
+const stateCode= $("#state").val();
+//const maxResults= $("#checked").val();
+
+const url = `https://developer.nps.gov/api/v1/parks?stateCode=${stateCode}&limit=10&api_key=MMqVQEXEFsuN6DmjKGPbOnM7cTqe6hn94ekdewXy`;
+
+let store =[]
+const main =function() {
+  console.log('DOM is loaded');
   //$('#content-box').append(startMsg);
-  
+
     fetch(url)
       .then(response => response.json())
-      .then(responseJson => { console.log(responseJson)
-          let image= responseJson.message;
+      .then(results =>{ console.log(results);
+        //console.log(results.data.fullName);
+    for(let i=0; i<results.data.length; i++){
+    const output = {name: results.data[i].fullName};
+    //console.log(output);
+    store.push(output);
+    
+    //$("#content").append(`${output.name}`)
+       }
+       console.log(store);
+       return store;
+    });
+    generateHTML()
+    renderList()
+  }; 
 
-          for(let i=0; i<image.length; i++){
+
   
-      $("#content").append("test"});
+function generateHTML(){
+  let html = [];
+  for(let i=0; i<store.length; i++){
+    html.push(`<li>${store[i].name}</li>`);
   }
+  console.log(html);
+  return html;
+}
+
+function renderList(){
+  let html = generateHTML();
+  console.log(html);
+  $('#content').html(html);
+}
+
+
 
 
 function watchForm() {
     $('form').submit(event => {
-        event.preventDefault();
-        main();
-        });
-    }
+      event.preventDefault();
+      main();
+
+    });
+  }
+
+
+
     
     $(function() {
-        console.log('App loaded! Waiting for submit!');
-        watchForm();
-      });
-
+      console.log('App loaded! Waiting for submit!');
+      watchForm();
+      //generateHTML;
+    });
